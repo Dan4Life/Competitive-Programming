caaeb20f6f95e301289e93186d4a47c041502888
Daniel Emeka-Ilozor <67161460+Dan4Life@users.noreply.github.com>
2023-10-02 22:18:11 +0100

    Update basic_grade_system.cpp

--- Random Mini Projects/basic_grade_system.cpp
+++ Random Mini Projects/basic_grade_system.cpp
@@ -1,26 +1,3 @@
-/**
-Basic Student Grade Management System:
-Math, English, Science
-0. Exit the program
-1. Add a student to the System
-     - Ask for name
-     - Ask for the grades
-
-2. Modify a student's info
-3. Show a student's report card
-4. Show all students' report card
-5. Delete a student in the system
-6. Delete all students in the system
-
-Inside report card:
-    average grade, total grade / 300
-    the score for each subject: A,B,C,D,E,F
-    the overall score: A--F
-
-No classes/structs involved.
-Allows for saving to database
-**/
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -33,78 +10,59 @@ float math_score[max_size];
 float english_score[max_size];
 float science_score[max_size];
 int numStudents = 0;
-char* database_file = "database.txt";
+string database_file = "database.txt";
 
-void add_to_file(int id){
-    ofstream file_output(database_file, ios::app);
-    file_output << full_name[id] << "\n";
-    file_output << math_score[id] << "\n";
-    file_output << english_score[id] << "\n";
-    file_output << science_score[id] << "\n";
-    file_output.close();
+void add_to_file(int id){ // Add a student to file
+    ofstream file_out(database_file, ios::app);
+    file_out << full_name[id] << "\n";
+    file_out << math_score[id] << "\n";
+    file_out << english_score[id] << "\n";
+    file_out << science_score[id] << "\n";
+    file_out.close();
 }
 
-void load_file(){
-    ofstream file_output(database_file, ios::app);
-    file_output.close(); numStudents = 0;
-    ifstream file_input(database_file);
+void update_file(int id=-1){ // possible to delete a certain ID
+    ofstream file_out(database_file);
+    for(int i = 0; i < numStudents; i++){
+        if(i==id) continue;
+        file_out << full_name[i] << "\n";
+        file_out << math_score[i] << "\n";
+        file_out << english_score[i] << "\n";
+        file_out << science_score[i] << "\n";
+    }
+    file_out.close();
+}
+
+void load_file(){ // from file to arrays
+    ofstream file_out(database_file, ios::app);
+    file_out.close(); numStudents = 0;
+    ifstream file_in(database_file);
     while(true){
-        getline(file_input, full_name[numStudents]);
-        if(file_input.eof()) break;
-        file_input >> math_score[numStudents];
-        file_input >> english_score[numStudents];
-        file_input >> science_score[numStudents];
-        if(!file_input.eof()) file_input.ignore();
+        getline(file_in, full_name[numStudents]);
+        if(file_in.eof()) break; // no input again
+        file_in >> math_score[numStudents];
+        file_in >> english_score[numStudents];
+        file_in >> science_score[numStudents];
+
+        // that extra '\n' is annoying for the next getline
+        if(!file_in.eof()) file_in.ignore();
         numStudents++;
     }
-    file_input.close();
-}
-
-void save_file(int id=-1){ // can save a file without a specific id
-    char* tempfile_name = "tempfile.txt";
-
-    ofstream tempfile(tempfile_name);
-    for(int i = 0; i < numStudents; i++){
-        if(i==id) continue;
-        tempfile << full_name[i] << "\n";
-        tempfile << math_score[i] << "\n";
-        tempfile << english_score[i] << "\n";
-        tempfile << science_score[i] << "\n";
-    }
-    tempfile.close(); remove(tempfile_name);
-
-    ofstream file_output(database_file);
-    for(int i = 0; i < numStudents; i++){
-        if(i==id) continue;
-        file_output << full_name[i] << "\n";
-        file_output << math_score[i] << "\n";
-        file_output << english_score[i] << "\n";
-        file_output << science_score[i] << "\n";
-    }
-    file_output.close();
+    file_in.close();
 }
 
 template<class T> void get_input(T &var){
-    while(true){
+    while(1){
         cin >> var;
         if(cin.fail()){
-            cin.clear(); // reset fail state flag
-		  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
         else{
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            if(cin.gcount() <= 1) break; // valid number
+            if(cin.gcount() <= 1) break;
         }
-        cout << "Invalid input. Please try again: ";
-    }
-}
-
-void validate(float &var){
-    while(true){
-        get_input(var);
-        if(var<0 or var>100)
-            cout << "Score not in range[0,100]. Please try again: ";
-        else break;
+        cout << "Invalid input, try again: ";
     }
 }
 
@@ -122,34 +80,38 @@ int getId(){
 
 void print_menu(){
     cout << "\n";
-    cout << "Welcome to our Grade Management System\n";
+    cout << "Welcome to our Grade System\n";
     cout << "0. Exit\n";
-    cout << "1. Add a student to the system\n";
+    cout << "1. Add a student to the System\n";
     cout << "2. Modify a student's info\n";
     cout << "3. Show a student's report card\n";
     cout << "4. Show all students' report card\n";
-    cout << "5. Delete a student in the system\n";
-    cout << "6. Delete all students in the system\n";
+    cout << "5. Delete a student's info in the system\n";
+    cout << "6. Delete all students' info in the system\n";
     cout << "\n";
 }
 
-int get_choice(){
-    int choice;
-    cout << "Enter your choice: ";
-    get_input(choice);
-    return choice;
+void validate(float &var){
+    while(true){
+        get_input(var);
+        if(var<0 or var>100)
+            cout << "Score not in range[0,100]. Please try again: ";
+        else break;
+    }
 }
 
 void add_student(){
     cout << "\nThis is to add a student\n";
     cout << "Enter the student's full name: ";
 
+    string s = full_name[numStudents];
     while(true){
-        getline(cin, full_name[numStudents]);
-        if(full_name[numStudents].size() > max_name_size)
+        getline(cin, s);
+        if(s.size() > max_name_size)
             cout << "Name is longer than " << max_name_size << " . Please try again: ";
         else break;
     }
+    full_name[numStudents] = s;
 
     cout << "Enter the student's math score: ";
     validate(math_score[numStudents]);
@@ -183,12 +145,14 @@ bool modify_student(){
 
     char option = verifyEdit("full name");
     if(option=='y'){
+        string s = full_name[id];
         while(true){
-            getline(cin, full_name[id]);
-            if(full_name[id].size() > max_name_size)
+            getline(cin, s);
+            if(s.size() > max_name_size)
                 cout << "Name is longer than " << max_name_size << " . Please try again: ";
             else break;
         }
+        full_name[id] = s;
         is_modified = true;
     }
     else if(option!='n') return is_modified;
@@ -235,7 +199,8 @@ void show_report_card(){
     }
 
     cout << "\nThis is to show a student's report card\n\n";
-    int id = getId(); if(id==-1) return;
+    int id = getId();
+    if(id==-1) return;
 
     cout << "Student's Full Name: " << full_name[id] << "\n";
     cout << "Student's Math Score: " << math_score[id] << " (" << grade_letter(math_score[id]) << ")\n";
@@ -293,16 +258,15 @@ void delete_student(){
         cout << "\nThere are no students in this system\n";
         return;
     }
-
     cout << "\nThis is to delete a student's data\n\n";
     int id = getId(); if(id==-1) return;
     cout << "Student name is " << full_name[id] << "\n\n";
     cout << "Are you really sure you want to delete this student id?\n";
     cout << "Respond with y/n (yes/no): ";
     char option; get_input(option);
-    if(option != 'y') return;
-    save_file(id); load_file();
-    cout << "Successfully deleted the ID\n";
+    if(option!='y') return;
+    update_file(id), load_file(); // delete from file, then load new file to arrays
+    cout << "Successfully deleted student\n";
 }
 
 void delete_all_students(){
@@ -310,27 +274,29 @@ void delete_all_students(){
         cout << "\nThere are no students in this system\n";
         return;
     }
-
     cout << "\nThis is to delete all students' data\n\n";
     cout << "Are you really sure you want to delete all students' data?\n";
     cout << "Respond with y/n (yes/no): ";
     char option; get_input(option);
-    if(option != 'y') return;
+    if(option!='y') return;
     string confirm = "delete|all", input;
     cout << "To confirm, please type the following: " << confirm << "\n";
-    getline(cin, input);
-    if(input!=confirm) return;
-    remove(database_file); load_file();
-    cout << "Successfully cleared the database\n";
+    cin >> input; if(input!=confirm) return;
+    ofstream file_out(database_file);
+    file_out.close(); load_file(); // clear file, then load new(empty) file to arrays
+    cout << "Successfully deleted all students\n";
 }
 
-int main(){
-    cout << fixed << setprecision(1);
+int main()
+{
+    cout << fixed << setprecision(2);
     load_file();
-
     while(true){
         print_menu();
-        int option = get_choice();
+        int option;
+        cout << "Enter your choice: ";
+        get_input(option);
+
         if(option==0){
             cout << "Thanks for visiting. See you next time!\n";
             return 0;
@@ -338,7 +304,7 @@ int main(){
         else if(option==1) add_student();
         else if(option==2){
             if(modify_student()){
-                save_file();
+                update_file();
                 cout << "Student info has successfully been modified\n";
             }
         }
