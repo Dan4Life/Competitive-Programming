03297ab5de6ffa2d77ed0dc807a4e0f4b64b05ec
Daniel Emeka-Ilozor <67161460+Dan4Life@users.noreply.github.com>
2023-12-12 20:54:25 +0100

    Update basic_quiz_application.cpp

--- Random Mini Projects/basic_quiz_application.cpp
+++ Random Mini Projects/basic_quiz_application.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 const int max_size = 1000;
+const int min_options = 2;
 const int max_options = 5;
 string database_file = "database.txt";
 
@@ -14,7 +15,7 @@ struct Question{
 vector<Question> questions;
 
 void add_to_file(Question &question){ // Add a question to file
-    ofstream file_out(database_file, ios::app);
+    ofstream file_out(database_file, ios::app); // create the file if it hasn't already, else just append to the file's contents
     file_out << question.statement << "\n";
     file_out << question.options.size() << "\n";
     for(auto option : question.options)
@@ -22,7 +23,7 @@ void add_to_file(Question &question){ // Add a question to file
     file_out.close();
 }
 
-void load_file(){ // from file to vectors
+void load_file(){ // from file to vector
     ofstream file_out(database_file, ios::app);
     file_out.close(); questions.clear();
     ifstream file_in(database_file);
@@ -43,7 +44,7 @@ void load_file(){ // from file to vectors
     file_in.close();
 }
 
-void update_file(){
+void update_file(){ // rewrite entire file with contents of the vector
     ofstream file_out(database_file);
     for(auto question : questions){
         file_out << question.statement << "\n";
@@ -54,7 +55,7 @@ void update_file(){
     file_out.close();
 }
 
-template<class T> void get_input(T &var){
+template<class T> void get_input(T &var){ // to prevent unwanted input during runtime
     T var2;
     while(1){
         cin >> var2;
@@ -71,7 +72,7 @@ template<class T> void get_input(T &var){
     var = var2;
 }
 
-void validate(int &var, int low, int high){
+void validate(int &var, int low, int high){ // to make sure integers are inputted in a certain range
     int var2;
     while(true){
         get_input(var2);
@@ -94,11 +95,28 @@ void print_menu(){
     cout << "(7). Take the test\n";
 }
 
+void show_question(int index){
+    if(index<0 or index>=questions.size()) return;
+    cout << "\nThe question statement is: \"" << questions[index].statement << "\"\n";
+    cout << "Current options:\n";
+    int cnt2 = 0;
+    for(auto option : questions[index].options){
+        cout << "(" << (char)('A'+cnt2) << "). " << option << "\n";
+        cnt2++;
+    }
+}
+
 void add_question(){
+    if(questions.size()>=max_size){
+        cout << "\nThe number of questions in the database has reached the maximum size (" << max_size << ")\n\n";
+        return;
+    }
+
     cout << "\nPlease input a question statement:\n";
     string statement; getline(cin, statement);
-    cout << "How many options do you want to add(2 to " << max_options << "): ";
-    int num_options; validate(num_options,2,max_options);
+
+    cout << "\nHow many options do you want to add (" << min_options << " to " << max_options << "): ";
+    int num_options; validate(num_options, min_options, max_options);
 
     vector<string> options; options.clear();
     for(int i = 0; i < num_options; i++){
@@ -107,24 +125,136 @@ void add_question(){
         string option; getline(cin, option);
         options.emplace_back(option);
     }
+
     questions.push_back(Question{statement, options});
     add_to_file(questions.back());
+    cout << "\nSuccessfully added new question!\n";
 }
 
 void delete_question(){
+    if(questions.empty()){
+        cout << "Database is empty!\n";
+        return;
+    }
 
+    cout << "There are currently " << questions.size() << " questions in the database\n";
+    cout << "Please input the index of the question you want to delete: ";
+    int index; validate(index,1,questions.size()); index--;
+
+    show_question(index);
+
+    cout << "\nAre you really sure you want to delete this question? Y/N: ";
+    char choice; get_input(choice);
+
+    if(choice=='Y'){
+        cout << "\nDeleting...\n";
+        questions.erase(questions.begin()+index);
+        update_file();
+        cout << "Successfully deleted this question!\n";
+        return;
+    }
 }
 
 void add_options_to_question(){
+    if(questions.empty()){
+        cout << "Database is empty!\n";
+        return;
+    }
 
+    cout << "There are currently " << questions.size() << " questions in the database\n";
+    cout << "Please input the index of the question you want to add options to: ";
+    int index; validate(index,1,questions.size()); index--;
+
+    int new_options = max_options-questions[index].options.size();
+    if(new_options<=0){
+        cout << "\nMaximum number of options added!\n";
+        return;
+    }
+
+    show_question(index);
+
+    cout << "\nYou are allowed to add at most " << new_options << " new option(s) to this question\n";
+    cout << "How many options do you want to add: ";
+    int num_options; validate(num_options, 0, new_options);
+    if(num_options==0) return;
+
+    vector<string> options; options.clear();
+    for(int i = 0; i < num_options; i++){
+        cout << "Enter option (" << (char)('A'+i+questions[index].options.size()) << "): ";
+        string option; getline(cin, option);
+        options.emplace_back(option);
+    }
+    for(auto option : options)
+        questions[index].options.emplace_back(option);
+    update_file();
+    cout << "\nSuccessfully added new options!\n";
 }
 
 void delete_option_from_question(){
+    if(questions.empty()){
+        cout << "\nDatabase is empty!\n";
+        return;
+    }
 
+    cout << "There are currently " << questions.size() << " questions in the database\n";
+    cout << "Please input the index of the question you want to delete an option from: ";
+    int index; validate(index,1,questions.size()); index--;
+
+    show_question(index);
+
+    if(questions[index].options.size()==min_options){
+        cout << "\nQuestion has minimum number of options required!\n";
+        return;
+    }
+
+    cout << "\nPlease input the index of the option you want to delete.\n";
+    cout << "Note that the first option which is assumed to be correct cannot be deleted: ";
+    int index2; validate(index2,2,questions[index].options.size()); index2--;
+
+    cout << "Are you sure you want to delete this option? Y/N: ";
+    char choice; get_input(choice);
+
+    if(choice=='Y'){
+        cout << "\nDeleting...\n";
+        questions[index].options.erase(questions[index].options.begin()+index2);
+        update_file();
+        cout << "Successfully deleted this question!\n";
+        return;
+    }
 }
 
 void edit_question(){
+    if(questions.empty()){
+        cout << "\nDatabase is empty!\n";
+        return;
+    }
 
+    cout << "There are currently " << questions.size() << " questions in the database\n";
+    cout << "Please input the index of the question you want to edit: ";
+    int index; validate(index,1,questions.size()); index--;
+
+    show_question(index);
+
+    cout << "\nAre you really sure you want to edit this question? Y/N: ";
+    char choice; get_input(choice);
+    if(choice!='Y') return;
+
+    cout << "\nPlease input the new question statement:\n";
+    string statement; getline(cin, statement);
+
+    cout << "\nHow many options do you want to add (" << min_options << " to " << max_options << "): ";
+    int num_options; validate(num_options, min_options, max_options);
+
+    vector<string> options; options.clear();
+    for(int i = 0; i < num_options; i++){
+        if(!i) cout << "Enter the correct option: ";
+        else cout << "Enter option (" << (char)('A'+i) << "): ";
+        string option; getline(cin, option);
+        options.emplace_back(option);
+    }
+
+    questions[index] = Question{statement, options};
+    update_file(); cout << "\nSuccessfully updated this question!\n";
 }
 
 void show_all_questions(){
@@ -135,9 +265,10 @@ void show_all_questions(){
         cout << cnt << ". " << question.statement << "\n";
         int cnt2 = 0;
         for(auto option : question.options){
-            cout << (char)('a'+cnt2) << "). " << option << "\n"; cnt2++;
+            cout << "(" << (char)('A'+cnt2) << "). " << option << "\n";
+            cnt2++;
         }
-        cout<<"\n"; cnt++;
+        cout<<"\n\n"; cnt++;
     }
     cout << "---------------------------------------------------------------------------\n";
 }
@@ -165,6 +296,6 @@ int main(){
         else if(choice==6) show_all_questions();
         else if(choice==7) start_test();
         else cout << "Your choice is not valid. Try again!";
-        cout << "\n";
+        cout << "\n\n";
     }
 }
