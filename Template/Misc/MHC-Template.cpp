21b0316b66fe455c291f2e0f18e05a4ce706287f
Daniel Emeka-Ilozor <67161460+Dan4Life@users.noreply.github.com>
2025-10-31 15:34:45 +0100

    Create MHC-Template.cpp

---
+++ Template/Misc/MHC-Template.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+//#define int long long
+#define sz(a) (int)a.size()
+#define all(a) begin(a),end(a)
+
+using ll = long long;
+using vi = vector<int>;
+using ar2 = array<int,2>;
+using ar3 = array<int,3>;
+
+const int mxN = (int)3e5+10;
+const int INF = (int)1e9+10;
+const ll LINF = (ll)4e18+10;
+const int MOD = (int)1e9+7;
+
+void solve(int t){
+    int ans;
+    
+	cout << "Case #" << t << ": ";
+	cout << ans << "\n";
+}
+
+// remember to " ulimit -s 1048576 " before compiling!
+ 
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int t; cin >> t;
+    for(int i = 1; i <= t; i++) solve(i);
+}
