301b96b1e0a460cdb342e0526869eb9712d63f40
Daniel Emeka-Ilozor <67161460+Dan4Life@users.noreply.github.com>
2026-02-23 08:07:12 +0200

    Create Template.cpp

---
+++ Template/Misc/Template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define pb push_back
+//#define int long long
+#define sz(a) (int)a.size()
+#define all(a) begin(a),end(a)
+
+using ll = long long;
+using vi = vector<int>;
+using ar2 = array<int,2>;
+using ar3 = array<int,3>;
+
+constexpr int mxN = (int)3e5+10;
+constexpr int INF = (int)1e9+10;
+constexpr ll LINF = (ll)4e18+10;
+constexpr int MOD = (int)1e9+7;
+
+void solve(){
+
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int t = 1; cin >> t;
+    while(t--) solve();
+}
