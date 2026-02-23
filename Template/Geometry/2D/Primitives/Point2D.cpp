6946a52c20d83df5e1769b34fe66c8b605e2f320
Daniel Emeka-Ilozor <67161460+Dan4Life@users.noreply.github.com>
2026-02-23 08:12:48 +0200

    Update and rename Point2D to Point2D.cpp

--- Template/Geometry/2D/Primitives/Point2D.cpp
+++ Template/Geometry/2D/Primitives/Point2D.cpp
@@ -6,14 +6,17 @@ No complex
 template<class T>
 struct Point2D{
     T x, y;
-
+    
     Point2d(T x=T{}, T y=T{}) : x(x),y(y) {}
 
+    bool operator<(const Point2D& other) const { return x<other.x||(x==other.x&&y<other.y); }
+    bool operator==(const Point2D& other) const { return x==other.x and y==other.y; }
+    
     Point2d& operator+=(const Point2d &P) { x+=P.x, y+=P.y; return *this; }
     Point2d& operator-=(const Point2d &P) { x-=P.x, y-=P.y; return *this; }
     Point2d& operator*=(const T &t) { x*=t, y*=t; return *this; }
     Point2d& operator/=(const T &t) { x/=t, y/=t; return *this; }
-
+    
     Point2d operator+(const Point2d &P) const{ return Point2d(*this)+=P; }
     Point2d operator-(const Point2d &P) const{ return Point2d(*this)-=P; }
     Point2d operator*(const T &t) const{ return Point2d(*this)*=t; }
@@ -21,7 +24,8 @@ struct Point2D{
 
     T dotP(const Point2d &P={}) const{ return x*P.x+y*P.y; }
     T crossP(const Point2d &P={}) const{ return x*P.y-P.x*y; }
-    T distance(const Point2d &P={}) const{ return hypot(P.x-x,P.y-y); }
+    T distSq(const Point2D &P = {}) const { return (P.x-x)*(P.x-x)+(P.y-y)*(P.y-y); }
+    T dist(const Point2d &P={}) const{ return hypot(P.x-x,P.y-y); }
 
     friend istream& operator>>(istream& in, Point2d &P){ return in >> P.x >> P.y;  }
     friend ostream& operator<<(ostream& out, const Point2d &P){ return out << P.x << " " << P.y;  }
